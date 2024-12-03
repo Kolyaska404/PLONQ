9d858f1a6d76026d8b76f61a1b0a8802e000635d
Nikolay Didenko <147000338+Kolyaska404@users.noreply.github.com>
2024-12-03 17:25:43 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+ymaps.ready(init);
+
+function init () {
+    var myMap = new ymaps.Map('map', {
+            center: [56.76, 51.64],
+            zoom: 4,
+            controls: []
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        objectManager = new ymaps.ObjectManager({
+            // Чтобы метки начали кластеризоваться, выставляем опцию.
+            clusterize: true,
+            // ObjectManager принимает те же опции, что и кластеризатор.
+            gridSize: 32,
+            clusterDisableClickZoom: true
+        });
+
+    // Чтобы задать опции одиночным объектам и кластерам,
+    // обратимся к дочерним коллекциям ObjectManager.
+    objectManager.objects.options.set('preset', 'islands#whiteIcon');
+    objectManager.clusters.options.set('preset', 'islands#whiteIcon');
+    myMap.geoObjects.add(objectManager);
+
+    $.ajax({
+        url: "data.json"
+    }).done(function(data) {
+        objectManager.add(data);
+    });
+}
